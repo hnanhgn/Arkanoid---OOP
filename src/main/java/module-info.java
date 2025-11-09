f72cc81f881de7c51af1ef3f93216f6589cf9287
NDTD19 <24020088@vnu.edu.vn>
2025-11-10 01:35:47 +0700

    add sound and music v2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.arkanoid {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.media;
 
     opens com.arkanoid.game.ui to javafx.fxml;
     exports com.arkanoid;
